7a0cc78f8c406f79eb916642165ab2defd476cd8
moniqueemh <mohernandez9992@yahoo.com>
2021-02-01 01:28:03 -0800

    created paths to employee and search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Employee from './components/pages/employee.js';
+import NavTabs from './components/NavTabs';
+import Search from './components/pages/search';
 import './App.css';
+//import { Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Employee} />
+        <Route exact path="/search" component={Search} />
+      </div>
+    </Router>
   );
 }
 
